839e38c74086ca174cd43a0e24aac78a74ed12e5
salty <salty@salty.dk>
2021-07-01 01:03:57 +0200

    Revert "Scripts: Removed migration from old ansible installs."
    This reverts commit 29a381b27f167cd20816839ec9ca92a46ed07a91.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 -m pip uninstall -y ansible
+python -m pip uninstall -y ansible ansible-base
+python3 -m pip uninstall -y ansible ansible-base
 python3 -m pip install ansible">=4.0.0,<5.0.0"
