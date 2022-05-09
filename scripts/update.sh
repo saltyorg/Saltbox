9e10c4e7e9ede8bc32331980d213235af7b517eb
salty <salty@salty.dk>
2022-05-09 12:31:01 +0200

    sanity_check: pipe ansible update script to log file

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 -m pip uninstall -y ansible ansible-base
-python3 -m pip install ansible">=5.0.0,<6.0.0"
+printf '' > /srv/git/saltbox/ansible-update.log
+python3 -m pip uninstall -y ansible ansible-base 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
+python3 -m pip install ansible">=5.0.0,<6.0.0" 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
