2ce301700bf65e6de31277fe9b7e9199bdd1d9d9
salty <salty@salty.dk>
2022-05-09 12:40:15 +0200

    scripts: Disable pip caching when updating Ansible

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 printf '' > /srv/git/saltbox/ansible-update.log
-python3 -m pip uninstall -y ansible ansible-base 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
-python3 -m pip install ansible">=5.0.0,<6.0.0" 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
+python3 -m pip uninstall -y --no-cache-dir --disable-pip-version-check ansible ansible-base 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
+python3 -m pip install --no-cache-dir --disable-pip-version-check ansible">=5.0.0,<6.0.0" 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
