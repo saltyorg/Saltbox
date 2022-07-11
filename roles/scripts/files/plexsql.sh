89829aed3570340a53d92184bf3ba58d69203a25
owine <owine@users.noreply.github.com>
2022-07-11 09:22:17 -0500

    Scripts: Add plexsql.sh

---
+++ roles/scripts/files/plexsql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# if script name is plexsql.sh then
+# usage is - ./plexsql.sh "select something from some_table where something = something_else"
+# 
+mkdir -p /opt/plexsql
+sqlplex="/opt/plexsql/Plex\ Media\ Server --sqlite"
+docker stop plex
+docker cp plex:/usr/lib/plexmediaserver/ /opt/plexsql
+cd "/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
+cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
+"$sqlplex" com.plexapp.plugins.library.db "$1"
+
+#and if you want to do something other than select statements use this instead
+#cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
+#"$sqplex" com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
+#"$sqplex" com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
+#"$sqplex" com.plexapp.plugins.library.db "$1"
