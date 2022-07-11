cc6e7a036e8df453fcd400d69dd89e851f0b564a
owine <owine@users.noreply.github.com>
2022-07-11 14:50:38 -0500

    Scripts: Fix plexsql.sh for real

--- roles/scripts/files/plexsql.sh
+++ roles/scripts/files/plexsql.sh
@@ -3,7 +3,7 @@
 # usage is - ./plexsql.sh "select something from some_table where something = something_else"
 # 
 mkdir -p /opt/plexsql
-sqlplex="/opt/plexsql/Plex\ Media\ Server --sqlite"
+sqlplex="/opt/plexsql/Plex SQLite"
 docker stop plex
 docker cp plex:/usr/lib/plexmediaserver/. /opt/plexsql
 cd "/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
@@ -12,6 +12,6 @@ cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
 
 #and if you want to do something other than select statements use this instead
 #cp com.plexapp.plugins.library.db com.plexapp.plugins.library.db.original
-#"$sqplex" com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
-#"$sqplex" com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
-#"$sqplex" com.plexapp.plugins.library.db "$1"
+#"$sqlplex" com.plexapp.plugins.library.db "DROP index 'index_title_sort_naturalsort'"
+#"$sqlplex" com.plexapp.plugins.library.db "DELETE from schema_migrations where version='20180501000000'"
+#"$sqlplex" com.plexapp.plugins.library.db "$1"
