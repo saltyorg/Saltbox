b664f48ec0592d1a2e90eb5018b63b3f49ebf780
salty <salty@salty.dk>
2022-09-27 04:25:07 +0200

    update.sh changes

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 printf '' > /srv/git/saltbox/ansible-update.log
-python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade --force-reinstall ansible">=6.0.0,<7.0.0" 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
+python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ansible">=6.0.0,<7.0.0" 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
