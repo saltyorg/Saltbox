db76b1595189dbc6041efe214920b8275b20aca1
salty <salty@salty.dk>
2022-09-27 03:56:38 +0200

    simplify ansible upgrade script to minimize issues

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 printf '' > /srv/git/saltbox/ansible-update.log
-python3 -m pip uninstall -y --no-cache-dir --disable-pip-version-check ansible ansible-base 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
-python3 -m pip install --no-cache-dir --disable-pip-version-check ansible">=6.0.0,<7.0.0" 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
+python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade --force-reinstall ansible">=6.0.0,<7.0.0" 1>>/srv/git/saltbox/ansible-update.log 2>>/srv/git/saltbox/ansible-update.log
