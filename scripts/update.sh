3a52b9cc1b9f20d8c16d8314a561e9c2f12bcb19
salty <salty@salty.dk>
2022-12-19 13:19:42 +0100

    use venv for ansible

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 printf '' > /srv/git/saltbox/ansible-update.log
-python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ansible">=6.0.0,<7.0.0" 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
+/srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pyOpenSSL requests netaddr jmespath jinja2 ansible">=6.0.0,<7.0.0" 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
+cp /srv/ansible/venv/bin/ansible* /usr/local/bin/
