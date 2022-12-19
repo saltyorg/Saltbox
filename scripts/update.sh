51bb9c9a0c394dd0f6a6e0a1c95456b20fce6d4c
salty <salty@salty.dk>
2022-12-19 18:55:12 +0100

    scripts: make update.sh silent if no errors occur. Everything is still logged to /srv/git/saltbox/ansible-update.log

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 printf '' > /srv/git/saltbox/ansible-update.log
-python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade apprise certbot 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
-/srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pip setuptools wheel 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
-/srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pyOpenSSL requests netaddr jmespath jinja2 ansible">=6.0.0,<7.0.0"  2>&1 | tee -a /srv/git/saltbox/ansible-update.log
-/srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ruamel.yaml tld argon2_cffi ndg-httpsclient 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
-/srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade dnspython lxml jmespath passlib PyMySQL 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
-/srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade docker 2>&1 | tee -a /srv/git/saltbox/ansible-update.log
-cp /srv/ansible/venv/bin/ansible* /usr/local/bin/
+{
+    python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade --root-user-action=ignore apprise certbot
+    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pip setuptools wheel
+    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pyOpenSSL requests netaddr jmespath jinja2 ansible">=6.0.0,<7.0.0"
+    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ruamel.yaml tld argon2_cffi ndg-httpsclient
+    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade dnspython lxml jmespath passlib PyMySQL
+    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade docker
+    cp /srv/ansible/venv/bin/ansible* /usr/local/bin/
+} >> /srv/git/saltbox/ansible-update.log 2>&1
+
+returnValue=$?
+
+if [ $returnValue -ne 0 ]; then
+    echo "An Error occurred check '/srv/git/saltbox/ansible-update.log' for more information."
+fi
+
+exit $returnValue
