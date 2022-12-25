b2669669385214f9bcf8dc6b6ea99d64e0eb0426
salty <salty@salty.dk>
2022-12-25 17:32:02 +0100

    scripts: remove docker pip module No longer a requirement for the ansible docker module.

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,7 +6,6 @@ printf '' > /srv/git/saltbox/ansible-update.log
     /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pyOpenSSL requests netaddr jmespath jinja2 ansible">=7.0.0,<8.0.0"
     /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ruamel.yaml tld ndg-httpsclient
     /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade dnspython lxml jmespath passlib PyMySQL
-    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade docker
     cp /srv/ansible/venv/bin/ansible* /usr/local/bin/
 } >> /srv/git/saltbox/ansible-update.log 2>&1
 
