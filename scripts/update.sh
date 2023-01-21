a0749cf73b9771bff053fac81bab46310e72bf1f
salty <salty@salty.dk>
2023-01-21 18:58:47 +0100

    shuffle pip install tasks back into roles

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 printf '' > /srv/git/saltbox/ansible-update.log
 {
-    python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade --root-user-action=ignore apprise certbot virtualenv
+    python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade --root-user-action=ignore certbot virtualenv
     /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pip setuptools wheel
     /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade pyOpenSSL requests netaddr jmespath jinja2 ansible">=7.0.0,<8.0.0"
-    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade ruamel.yaml tld argon2_cffi ndg-httpsclient
-    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade dnspython lxml jmespath passlib PyMySQL
-    /srv/ansible/venv/bin/python3 -m pip install --no-cache-dir --disable-pip-version-check --upgrade docker
     cp /srv/ansible/venv/bin/ansible* /usr/local/bin/
 } >> /srv/git/saltbox/ansible-update.log 2>&1
 
